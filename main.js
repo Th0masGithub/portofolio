c8bf4ce70dd302bc010ab8cc5ca055c47f868489
th0masGithub <thomasadberthuang@gmail.com>
2023-05-06 11:05:14 +0700

    tidy it up

--- main.js
+++ main.js
@@ -12,28 +12,22 @@ function toggleDetail(e) {
     $(detail).slideToggle()
 }
 
-function onFormSubmit(e){
-    e.preventsDefault()
+function onFormSubmit(e) {
+    e.preventDefault()
     const email = $("#inp_email")
     const subject = $("#inp_subject")
     const message = $("#inp_message")
 
-    if(!$(email).val()){
+    if(!$(email).val()) {
         alert("Email is required")
-    }
-
-    else if(!$(subject).val()){
+    } else if (!$(subject).val()) {
         alert("Subject is required")
-    }
-
-    else if(!$(message)){
-        alert("message is requeired")
-    }
-
-    else{
+    } else if (!$(message).val()) {
+        alert("Message is required")
+    } else {
+        alert("Form Submitted")
         $(email).val("")
         $(subject).val("")
         $(message).val("")
-        alert("Form submitted")
     }
 }
